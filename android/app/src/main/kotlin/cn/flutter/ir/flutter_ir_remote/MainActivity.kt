53a9e12c401c38ffb455b7ffaa9a9309949f94d2
xxnjdlys <xxnjdlys@gmail.com>
2020-09-16 16:32:53 +0800

    first commit

---
+++ android/app/src/main/kotlin/cn/flutter/ir/flutter_ir_remote/MainActivity.kt
@@ -0,0 +1,6 @@
+package cn.flutter.ir.flutter_ir_remote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AppColors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBackground = Colors.white;
+  static const Color lightText = Color(0xFF7E7E7E);
+  static const Color lightSelect = Colors.black;
+  static const Color lightIcon = Color(0xFF7E7E7E);
+  static const Color lightIconButton = Colors.black;
+  static const Color lightButtonBackground = Color(0xFFE7E7E7);
+
+  static const Color darkBackground = Color(0xFF141414);
+  static const Color darkext = Color(0xFFAFAFAF);
+  static const Color darkSelect = Colors.white;
+  static const Color darkIcon = Color(0xFF767676);
+  static const Color darkIconButton = Colors.white;
+  static const Color darkButtonBackground = Color(0xFF1F1F1F);
+
+  static const Color dpadDirectionBackground = Color(0xFF7174ff);
+  static const Color dpadDragHoverBackground = Color(0xAF000000);
+}
